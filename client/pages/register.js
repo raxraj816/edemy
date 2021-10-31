0af8717883b6128fc39235f31a11a4e415480142
raxraj816 <raxraj816@gmail.com>
2021-10-31 09:59:39 +0530

    Custom next dev server (for dev mode only)

--- client/pages/register.js
+++ client/pages/register.js
@@ -14,14 +14,11 @@ const Register = () => {
     // console.table({ name, email, password });
     try {
       setLoading(true);
-      const { data } = await axios.post(
-        `${process.env.NEXT_PUBLIC_API}/register`,
-        {
-          name,
-          email,
-          password,
-        }
-      );
+      const { data } = await axios.post(`/api/register`, {
+        name,
+        email,
+        password,
+      });
       // console.log("REGISTER RESPONSE", data);
       toast("Registration successful. Please login.");
       setLoading(false);
