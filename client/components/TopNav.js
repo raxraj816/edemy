f47b344031fa7bc7197cc320fda7ca7592d43e51
raxraj816 <raxraj816@gmail.com>
2021-10-31 18:39:05 +0530

    Logout

--- client/components/TopNav.js
+++ client/components/TopNav.js
@@ -1,20 +1,37 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useContext } from "react";
 import { Menu } from "antd";
 import Link from "next/link";
 import {
   AppstoreOutlined,
   LoginOutlined,
+  LogoutOutlined,
   UserAddOutlined,
 } from "@ant-design/icons";
+import { Context } from "../context";
+import axios from "axios";
+import { useRouter } from "next/router";
+import { toast } from "react-toastify";
 
 const { Item } = Menu;
 
 const TopNav = () => {
   const [current, setCurrent] = useState("");
 
+  const { state, dispatch } = useContext(Context);
+
+  const router = useRouter();
+
   useEffect(() => {
     process.browser && setCurrent(window.location.pathname);
   }, [process.browser && window.location.pathname]);
+
+  const logout = async () => {
+    dispatch({ type: "LOGOUT" });
+    window.localStorage.removeItem("user");
+    const { data } = await axios.get("/api/logout");
+    toast(data.message);
+    router.push("/login");
+  };
 
   return (
     <Menu mode="horizontal" selectedKeys={[current]}>
@@ -46,6 +63,15 @@ const TopNav = () => {
         <Link href="/register">
           <a>Register</a>
         </Link>
+      </Item>
+
+      <Item
+        key="/logout"
+        onClick={logout}
+        icon={<LogoutOutlined />}
+        className="float-right"
+      >
+        Logout
       </Item>
     </Menu>
   );
--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -61,3 +61,12 @@ export const login = async (req, res) => {
     return res.status(400).send("Error. Try again.");
   }
 };
+
+export const logout = async (req, res) => {
+  try {
+    res.clearCookie("token");
+    return res.json({ message: "Signout success" });
+  } catch (err) {
+    console.log(err);
+  }
+};
