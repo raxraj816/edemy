2bfbc0cba0eeaf27f297b54f63a1e142d0411a1b
raxraj816 <raxraj816@gmail.com>
2021-11-06 14:12:15 +0530

    Handling expired token using axios interceptors

--- client/context/index.js
+++ client/context/index.js
@@ -1,4 +1,6 @@
 import { useReducer, createContext, useEffect } from "react";
+import axios from "axios";
+import { useRouter } from "next/router";
 
 // initial state
 const intialState = {
@@ -24,12 +26,45 @@ const rootReducer = (state, action) => {
 const Provider = ({ children }) => {
   const [state, dispatch] = useReducer(rootReducer, intialState);
 
+  // router
+  const router = useRouter();
+
   useEffect(() => {
     dispatch({
       type: "LOGIN",
       payload: JSON.parse(window.localStorage.getItem("user")),
     });
   }, []);
+
+  axios.interceptors.response.use(
+    function (response) {
+      // any status code that lie within the range of 2XX cause this function
+      // to trigger
+      return response;
+    },
+    function (error) {
+      // any status codes that falls outside the range of 2xx cause this function
+      // to trigger
+      let res = error.response;
+      if (res.status === 401 && res.config && !res.config.__isRetryRequest) {
+        return new Promise((resolve, reject) => {
+          axios
+            .get("/api/logout")
+            .then((data) => {
+              console.log("/401 error > logout");
+              dispatch({ type: "LOGOUT" });
+              window.localStorage.removeItem("user");
+              router.push("/login");
+            })
+            .catch((err) => {
+              console.log("AXIOS INTERCEPTORS ERR", err);
+              reject(error);
+            });
+        });
+      }
+      return Promise.reject(error);
+    }
+  );
 
   return (
     <Context.Provider value={{ state, dispatch }}>{children}</Context.Provider>
